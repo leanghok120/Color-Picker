eeb0881ed68208dce83b96796a207297c374c823
Astrox <112290652+leanghok120@users.noreply.github.com>
2023-03-25 12:31:31 +0700

    added the files

---
+++ script.js
@@ -0,0 +1,22 @@
+function changeBackgroundColorRed() {
+    document.body.style.backgroundColor = "red";
+}
+
+function changeBackgroundColorGreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+function changeBackgroundColorBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function changeBackgroundColorYellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function changeBackgroundColorOrange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+
+
